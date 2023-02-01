717f71b9569b536e6b6956ed0d82d55b3e67eb9f
MaxBavon <maxou.etienne@gmail.com>
2023-02-01 13:49:25 +0100

    loop

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pygame
 
-print("Launching Game !")
+window = pygame.display.set_mode((512, 512))
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
